2fda620605c4c47f6d5eb21978085a1a3dd3d615
drajotte94 <drajotte@mygcu.edu>
2024-06-16 08:47:46 -0700

    Moved game logic into a back end game service handler and added the groundwork for a partial view to display upon win/lose condition being met

--- Controllers/MinesweeperController.cs
+++ Controllers/MinesweeperController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Milestone1_350.Controllers;
 using Milestone1_350.Models;
+using Milestone1_350.Services;
 
 namespace Milestone1_350.Controllers
 {
@@ -10,7 +11,7 @@ namespace Milestone1_350.Controllers
         Random random = new Random();
         const int GRID_SIZE = 36;
         BoardModel gameBoard = HomeController.gameBoard;
-
+        GameService gs = new GameService();
 
         public IActionResult Index()
         {
@@ -31,88 +32,19 @@ namespace Milestone1_350.Controllers
         {
             int bN = int.Parse(buttonNumber);
 
-            if (User.ElementAt(bN).ButtonState <= 1)
+            // update the button state to show the new image
+            if (User.ElementAt(bN).ButtonState == 0)
             {
-                // if the tile is at the default button state then place the flag
-                if (User.ElementAt(bN).ButtonState == 0)
-                {
-                    User.ElementAt(bN).ButtonState = (User.ElementAt(bN).ButtonState + 1);
-                }
-                else
-                {
-                    // if the tile has already been flagged
-                    // set the button state equal to the number of neighbors (the number of bomb tiles it is touching)
-                    // 2 is added to this value to compensate for unchecked at 0, and flagged at 1
-                    // example: if the tile has NumberOfNeighbors of 1 the buttonState would be set to 3 which displays the 1.png image
-                    User.ElementAt(bN).ButtonState = (User.ElementAt(bN).NumberOfNeighbors + 2);
-
-                    // perform flood fill action if the space has no dangerous neighbors
-                    if (User.ElementAt(bN).NumberOfNeighbors == 0)
-                    {
-                        int r = User.ElementAt(bN).Row;
-                        int c = User.ElementAt(bN).Col;
-                        floodFill(r, c);
-                    }
-                }
+                gs.revealSquare(User, gameBoard, bN);
             }
+
+            // check if the game is over and display the correct win/lose condition if it is
+            if (gs.gameOver(User, gameBoard) == true)
+            {
+                return PartialView("_gameover");
+            }
+
             return View("Index", User);
-        }
-
-        private void floodFill(int r, int c)
-        {
-            if (!gameBoard.Grid[r, c].Visited && gameBoard.isSafeCell(r, c))
-            {
-                gameBoard.Grid[r, c].Visited = true;
-
-                if (gameBoard.isSafeCell(r - 1, c)) // N
-                { 
-                    if (gameBoard.Grid[r - 1, c].LiveNeighbors == 0) floodFill(r - 1, c); 
-                    else gameBoard.Grid[r - 1, c].Visited = true;
-                    for (int i = 0; i < User.Count; i++) { if (User.ElementAt(i).Row == r - 1 && User.ElementAt(i).Col == c) { User.ElementAt(i).ButtonState = User.ElementAt(i).NumberOfNeighbors + 2; } }
-                }
-                if (gameBoard.isSafeCell(r - 1, c)) // NE
-                {
-                    if (gameBoard.Grid[r - 1, c].LiveNeighbors == 0) floodFill(r - 1, c + 1);
-                    else gameBoard.Grid[r - 1, c].Visited = true;
-                    for (int i = 0; i < User.Count; i++) { if (User.ElementAt(i).Row == r - 1 && User.ElementAt(i).Col == c) { User.ElementAt(i).ButtonState = User.ElementAt(i).NumberOfNeighbors + 2; } }
-                }
-                if (gameBoard.isSafeCell(r, c + 1)) // E
-                { 
-                    if (gameBoard.Grid[r, c + 1].LiveNeighbors == 0) floodFill(r, c + 1); 
-                    else gameBoard.Grid[r, c + 1].Visited = true;
-                    for (int i = 0; i < User.Count; i++) { if (User.ElementAt(i).Row == r && User.ElementAt(i).Col == c + 1) { User.ElementAt(i).ButtonState = User.ElementAt(i).NumberOfNeighbors + 2; } }
-                }
-                if (gameBoard.isSafeCell(r - 1, c)) // SE
-                {
-                    if (gameBoard.Grid[r - 1, c].LiveNeighbors == 0) floodFill(r - 1, c + 1);
-                    else gameBoard.Grid[r - 1, c].Visited = true;
-                    for (int i = 0; i < User.Count; i++) { if (User.ElementAt(i).Row == r - 1 && User.ElementAt(i).Col == c) { User.ElementAt(i).ButtonState = User.ElementAt(i).NumberOfNeighbors + 2; } }
-                }
-                if (gameBoard.isSafeCell(r + 1, c)) // S
-                { 
-                    if (gameBoard.Grid[r + 1, c].LiveNeighbors == 0) floodFill(r + 1, c); 
-                    else gameBoard.Grid[r + 1, c].Visited = true;
-                    for (int i = 0; i < User.Count; i++) { if (User.ElementAt(i).Row == r + 1 && User.ElementAt(i).Col == c) { User.ElementAt(i).ButtonState = User.ElementAt(i).NumberOfNeighbors + 2; } }
-                }
-                if (gameBoard.isSafeCell(r - 1, c)) // SW
-                {
-                    if (gameBoard.Grid[r - 1, c].LiveNeighbors == 0) floodFill(r + 1, c - 1);
-                    else gameBoard.Grid[r - 1, c].Visited = true;
-                    for (int i = 0; i < User.Count; i++) { if (User.ElementAt(i).Row == r - 1 && User.ElementAt(i).Col == c) { User.ElementAt(i).ButtonState = User.ElementAt(i).NumberOfNeighbors + 2; } }
-                }
-                if (gameBoard.isSafeCell(r, c - 1)) // W
-                { 
-                    if (gameBoard.Grid[r, c - 1].LiveNeighbors == 0) floodFill(r, c - 1); 
-                    else gameBoard.Grid[r, c - 1].Visited = true;
-                    for (int i = 0; i < User.Count; i++) { if (User.ElementAt(i).Row == r && User.ElementAt(i).Col == c - 1) { User.ElementAt(i).ButtonState = User.ElementAt(i).NumberOfNeighbors + 2; } }
-                }
-                if (gameBoard.isSafeCell(r - 1, c)) // NW
-                {
-                    if (gameBoard.Grid[r - 1, c].LiveNeighbors == 0) floodFill(r - 1, c - 1);
-                    else gameBoard.Grid[r - 1, c].Visited = true;
-                    for (int i = 0; i < User.Count; i++) { if (User.ElementAt(i).Row == r - 1 && User.ElementAt(i).Col == c) { User.ElementAt(i).ButtonState = User.ElementAt(i).NumberOfNeighbors + 2; } }
-                }
-            }
         }
     }
 }
---
+++ Services/GameService.cs
@@ -0,0 +1,120 @@
+﻿using Milestone1_350.Models;
+
+namespace Milestone1_350.Services
+{
+    public class GameService
+    {
+        public GameService() { }
+
+        public List<ButtonModel> revealSquare(List<ButtonModel> User, BoardModel gameBoard, int bN)
+        {
+            // set the button state equal to the number of neighbors (the number of bomb tiles it is touching)
+            // 2 is added to this value to compensate for unchecked at 0, and flagged at 1
+            // example: if the tile has NumberOfNeighbors of 1 the buttonState would be set to 3 which displays the 1.png image
+            User.ElementAt(bN).ButtonState = (User.ElementAt(bN).NumberOfNeighbors + 2);
+
+            // perform flood fill action if the space has no dangerous neighbors
+            if (User.ElementAt(bN).NumberOfNeighbors == 0)
+            {
+                int r = User.ElementAt(bN).Row;
+                int c = User.ElementAt(bN).Col;
+                floodfill(gameBoard, User, r, c);
+            }
+
+            return User;
+        }
+
+        public bool gameOver(List<ButtonModel> User, BoardModel gameBoard)
+        {
+            int safeTiles = 0;
+
+            for (int i = 0; i < User.Count(); i++)
+            {
+                // if the tile is a bomb and is revealed the game is over
+                if (User[i].ButtonState == 11)
+                {
+                    // reveal all other bombs on the board
+                    for (int j = 0; j < User.Count(); j++)
+                    {
+                        if (User[j].NumberOfNeighbors == 9)
+                        {
+                            User[j].ButtonState = 11;
+                        }
+                    }
+                    break;
+                }
+
+                // if tile is not revealed and is not a bomb then the game isn't over
+                if (User[i].ButtonState == 0 && User[i].NumberOfNeighbors != 9)
+                {
+                    safeTiles++;
+                }
+            }
+
+            if (safeTiles > 0)
+            {
+                return false;
+            } else
+            {
+                return true;
+            }
+        }
+
+        public void floodfill(BoardModel gameBoard, List<ButtonModel> User, int r, int c)
+        {
+            if (!gameBoard.Grid[r, c].Visited && gameBoard.isSafeCell(r, c))
+            {
+                gameBoard.Grid[r, c].Visited = true;
+
+                if (gameBoard.isSafeCell(r - 1, c)) // N
+                {
+                    if (gameBoard.Grid[r - 1, c].LiveNeighbors == 0) floodfill(gameBoard, User, r - 1, c);
+                    else gameBoard.Grid[r - 1, c].Visited = true;
+                    for (int i = 0; i < User.Count; i++) { if (User.ElementAt(i).Row == r - 1 && User.ElementAt(i).Col == c) { User.ElementAt(i).ButtonState = User.ElementAt(i).NumberOfNeighbors + 2; } }
+                }
+                if (gameBoard.isSafeCell(r - 1, c)) // NE
+                {
+                    if (gameBoard.Grid[r - 1, c].LiveNeighbors == 0) floodfill(gameBoard, User, r - 1, c + 1);
+                    else gameBoard.Grid[r - 1, c].Visited = true;
+                    for (int i = 0; i < User.Count; i++) { if (User.ElementAt(i).Row == r - 1 && User.ElementAt(i).Col == c) { User.ElementAt(i).ButtonState = User.ElementAt(i).NumberOfNeighbors + 2; } }
+                }
+                if (gameBoard.isSafeCell(r, c + 1)) // E
+                {
+                    if (gameBoard.Grid[r, c + 1].LiveNeighbors == 0) floodfill(gameBoard, User, r, c + 1);
+                    else gameBoard.Grid[r, c + 1].Visited = true;
+                    for (int i = 0; i < User.Count; i++) { if (User.ElementAt(i).Row == r && User.ElementAt(i).Col == c + 1) { User.ElementAt(i).ButtonState = User.ElementAt(i).NumberOfNeighbors + 2; } }
+                }
+                if (gameBoard.isSafeCell(r - 1, c)) // SE
+                {
+                    if (gameBoard.Grid[r - 1, c].LiveNeighbors == 0) floodfill(gameBoard, User, r - 1, c + 1);
+                    else gameBoard.Grid[r - 1, c].Visited = true;
+                    for (int i = 0; i < User.Count; i++) { if (User.ElementAt(i).Row == r - 1 && User.ElementAt(i).Col == c) { User.ElementAt(i).ButtonState = User.ElementAt(i).NumberOfNeighbors + 2; } }
+                }
+                if (gameBoard.isSafeCell(r + 1, c)) // S
+                {
+                    if (gameBoard.Grid[r + 1, c].LiveNeighbors == 0) floodfill(gameBoard, User, r + 1, c);
+                    else gameBoard.Grid[r + 1, c].Visited = true;
+                    for (int i = 0; i < User.Count; i++) { if (User.ElementAt(i).Row == r + 1 && User.ElementAt(i).Col == c) { User.ElementAt(i).ButtonState = User.ElementAt(i).NumberOfNeighbors + 2; } }
+                }
+                if (gameBoard.isSafeCell(r - 1, c)) // SW
+                {
+                    if (gameBoard.Grid[r - 1, c].LiveNeighbors == 0) floodfill(gameBoard, User, r + 1, c - 1);
+                    else gameBoard.Grid[r - 1, c].Visited = true;
+                    for (int i = 0; i < User.Count; i++) { if (User.ElementAt(i).Row == r - 1 && User.ElementAt(i).Col == c) { User.ElementAt(i).ButtonState = User.ElementAt(i).NumberOfNeighbors + 2; } }
+                }
+                if (gameBoard.isSafeCell(r, c - 1)) // W
+                {
+                    if (gameBoard.Grid[r, c - 1].LiveNeighbors == 0) floodfill(gameBoard, User, r, c - 1);
+                    else gameBoard.Grid[r, c - 1].Visited = true;
+                    for (int i = 0; i < User.Count; i++) { if (User.ElementAt(i).Row == r && User.ElementAt(i).Col == c - 1) { User.ElementAt(i).ButtonState = User.ElementAt(i).NumberOfNeighbors + 2; } }
+                }
+                if (gameBoard.isSafeCell(r - 1, c)) // NW
+                {
+                    if (gameBoard.Grid[r - 1, c].LiveNeighbors == 0) floodfill(gameBoard, User, r - 1, c - 1);
+                    else gameBoard.Grid[r - 1, c].Visited = true;
+                    for (int i = 0; i < User.Count; i++) { if (User.ElementAt(i).Row == r - 1 && User.ElementAt(i).Col == c) { User.ElementAt(i).ButtonState = User.ElementAt(i).NumberOfNeighbors + 2; } }
+                }
+            }
+        }
+    }
+}
